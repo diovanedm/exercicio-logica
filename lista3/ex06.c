1c872cb599ea9964eac4f86cfd6ed8727fbe6020
Diovane Maia <diovane.dm@gmail.com>
2021-01-12 15:35:25 -0300

    ex06.c feito

--- lista3/ex06.c
+++ lista3/ex06.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
-
+#include <math.h>
 int main(void) {
-    
+
+  float a, b, c, x, delta, y1, y2, numerador, denominador;
+
+  a = 1;
+  b = -1;
+  c = -12;
+
+  delta = powf(b, 2) - 4 * a * c;
+  printf("%f\n", delta);
+
+  denominador = 2 * a;
+
+  y1 = -b - sqrtf(delta)/denominador;
+  y2 = -b + sqrtf(delta)/denominador;
+
+  
+  printf("%.1f\n%1.f\n", y1, y2);
+  printf("%.f", denominador);
+
+  return 0;
 }
